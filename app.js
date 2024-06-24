d1091c2e6c5033ebae337c4c65ac5a53a92c4b18
Athoug <athoug.s@gmail.com>
2024-06-24 08:57:52 +0300

    add new route

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const path = require('path');
 
 const app = express();
 
-// serve static files from public directory
+// middleware to serve static files from public directory and projects
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/projects', express.static(path.join(__dirname, 'src', 'projects')));
 
 // route to serve the index.html
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/projects/:projectDay', (req, res) => {
+	const projectDay = req.params.projectDay;
+	const projectPath = path.join(
+		__dirname,
+		'src',
+		'projects',
+		projectDay,
+		'index.html'
+	);
+
+	console.log(projectPath);
+	res.sendFile(projectPath);
 });
 
 // start the server
