77e0c1952b2a27075359db04bc003cbc63fe7fc1
Athoug <athoug.s@gmail.com>
2024-06-28 18:37:23 +0300

    file clean up

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ const __dirname = path.dirname(__filename);
 // middleware to serve static files from public directory and projects
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/projects', express.static(path.join(__dirname, 'src', 'projects')));
-app.use(
-	'/project-public',
-	express.static(path.join(__dirname, 'src/projects/public'))
-);
 
 // route to serve the index.html
 app.get('/', (req, res) => {
